f29af4d3c7daad804db8767d19c39818adba15ce
xiaohei <1474344699@qq.com>
2022-11-25 11:34:22 +0800

    feat:herderのHTML+CSS

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,7 +1,32 @@
 <template>
-    <div>
-        <h2>头部</h2>
+    <div class="header">
+    <div class="wrap">
+      <div class="nav-left">
+        <div class="img">
+          <img src="https://rs.dance365.com/logo@3xrenew3.png" alt="" />
+        </div>
+        <RouterLink to="/recommend" class="item"  active-class="active" exact
+          >资源</RouterLink
+        >
+        <RouterLink to="/discovery/vip_zone" class="item">课程</RouterLink>
+        <RouterLink to="/training" class="item">练习室</RouterLink>
+        <RouterLink to="/communication/systemNote" class="item"
+          >交流</RouterLink
+        >
+        <RouterLink to="/center/dynamics" class="item">我的</RouterLink>
+      </div>
+      <div class="nav-right">
+        <div class="down"><span></span>下载APP</div>
+        <button class="btn">+ 发布</button>
+        <div class="avatar">
+          <span>登录</span>
+          |
+          <span>注册</span>
+          <!-- <img src="../../assets/default_avatar.png" alt="" /> -->
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -15,6 +40,81 @@ export default defineComponent({
 
 </script>
 
-<style scoped>
-
+<style scoped lang='less'>
+.header {
+  width: 100%;
+  height: 60px;
+  box-shadow: 0 0 10px 2px #ddd;
+  .wrap {
+    width: 1200px;
+    height: 60px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .nav-left {
+      height: 100%;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      img {
+        cursor: pointer;
+        width: 128px;
+        height: 30px;
+        line-height: 60px;
+      }
+      .item {
+        cursor: pointer;
+        width: 56px;
+        height: 60px;
+        line-height: 60px;
+        text-align: center;
+        margin-left: 40px;
+        font-size: 18px;
+        text-decoration: none;
+        color: black;
+        &.active {
+          color: #f93684;
+        }
+      }
+    }
+    .nav-right {
+      height: 60px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .down {
+        cursor: pointer;
+      }
+      .btn {
+        width: 75px;
+        height: 30px;
+        background-color: #f93684;
+        border-radius: 15px;
+        color: #fff;
+        font-size: 16px;
+        padding: 0;
+        margin: 0 28px 0 30px;
+        border: none;
+        cursor: pointer;
+      }
+      .avatar {
+        width: 80px;
+        height: 30px;
+        display: flex;
+        justify-content: space-between;
+        cursor: pointer;
+        .avat {
+          width: 30px;
+          height: 30px;
+        }
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+      }
+    }
+  }
+}
 </style>
