c708168f804023e86ff2d0b0060bb9b63a9a3c8c
jonathan <jonathan_workemail@qq.com>
2022-11-25 12:52:09 +0800

    将header组件注册为公共组件

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import headBar from '@/components/Header/index.vue'
 import router from './router';
 const app = createApp(App);
 // //全局注册组件
-// app.component('Header', Header);
+app.component('headBar', headBar);
 // app.component('Footer', Footer);
 app.use(router)
 .mount('#app');
