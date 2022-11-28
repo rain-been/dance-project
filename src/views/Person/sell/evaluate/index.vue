046371547b11ff52586a5ef5e10356ffe65ba370
zxp0430 <1055256760@qq.com>
2022-11-28 21:49:40 +0800

    修改

--- src/views/Person/sell/evaluate/index.vue
+++ src/views/Person/sell/evaluate/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-<<<<<<< HEAD
       <div class="right">
         <el-card class="topHeader">
           <div class="title">评价管理 ：</div>
@@ -131,23 +130,3 @@
   }
   </style>
   
-=======
-        <h2>evaluate</h2>
-    </div>
-</template>
-
-<script lang="ts">
-import {defineComponent} from 'vue'
-export default defineComponent({
-    name: 'evaluate'
-})
-</script>
-
-<script lang = "ts" setup>
-
-</script>
-
-<style scoped>
-
-</style>
->>>>>>> 857d9d6b9c906b5a357dd74d2d2680d45a800d8a
--- src/views/Person/sell/preferential/index.vue
+++ src/views/Person/sell/preferential/index.vue
@@ -362,23 +362,5 @@
   }
   </style>
   
-=======
-        <h2>preferential</h2>
-    </div>
-</template>
 
-<script lang="ts">
-import {defineComponent} from 'vue'
-export default defineComponent({
-    name: 'preferential'
-})
-</script>
 
-<script lang = "ts" setup>
-
-</script>
-
-<style scoped>
-
-</style>
->>>>>>> 857d9d6b9c906b5a357dd74d2d2680d45a800d8a
